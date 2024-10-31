1812918bda1310cd3e8aa1bbb7ce1d7a5059fd5b
sebastian-VB <sebas99peru@gmail.com>
2024-10-30 20:55:53 -0500

    Change in scale transition

--- src/app/sections/components/button-cv/button-cv.component.ts
+++ src/app/sections/components/button-cv/button-cv.component.ts
@@ -4,9 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-button-cv',
   template:
   `
-    <button class= "px-4 py-3 rounded-xl font-bold mt-6
-                    transition ease-in-out delay-150 bg-yellow-300
-                    hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300">
+    <button class= "bg-yellow-300 px-4 py-3 rounded-xl font-bold mt-6
+                  hover:bg-yellow-400">
       Descargar CV
     </button>
   `,
