c4eef64c433a685cb5908a384a8ba24077501957
sebastian-VB <sebas99peru@gmail.com>
2024-11-08 20:16:07 -0500

    Create list from projects

--- src/app/sections/projects-section/projects-section.component.ts
+++ src/app/sections/projects-section/projects-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../interfaces/project.interface';
 
 @Component({
   selector: 'app-projects-section',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class ProjectsSectionComponent {
 
+  public profileUrl: string = 'https://github.com/sebastian-VB';
+
+  public projectsList: Project[] = [
+    {
+      imageUrl: './images/img-projects/game_app.webp',
+      projectName: 'Games App',
+      technologies: ['HTML', 'CSS', 'TYPESCRIPT', 'ANGULAR', 'TAILWIND CSS'],
+      repositoryUrl: 'https://github.com/sebastian-VB/games-page'
+    },
+    {
+      imageUrl: './images/img-projects/project_backend.webp',
+      projectName: 'Sistema de exmenes - backend',
+      technologies: ['JAVA', 'SPRINGBOOT', 'JWT'],
+      repositoryUrl: 'https://github.com/sebastian-VB/testing-system-backend'
+    },
+    {
+      imageUrl: './images/img-projects/testing_system.webp',
+      projectName: 'Sistema de exmenes - frontend',
+      technologies: ['HTML', 'CSS', 'TYPESCRIPT', 'ANGULAR', 'ANGULAR MATERIAL'],
+      repositoryUrl: 'https://github.com/sebastian-VB/testing-system-frontend'
+    },
+    {
+      imageUrl: './images/img-projects/country_app.webp',
+      projectName: 'Country App',
+      technologies: ['HTML', 'CSS', 'TYPESCRIPT', 'ANGULAR', 'ANGULAR MATERIAL'],
+      repositoryUrl: 'https://github.com/sebastian-VB/country-app'
+    },
+    {
+      imageUrl: './images/img-projects/project_backend.webp',
+      projectName: 'Sistema para consultorio médico - backend',
+      technologies: ['JAVA', 'SPRINGBOOT', 'JWT'],
+      repositoryUrl: 'https://github.com/sebastian-VB/crud_medical_office_withWT'
+    },
+    {
+      imageUrl: './images/img-projects/hero_app.webp',
+      projectName: 'Heroes App - crud',
+      technologies: ['HTML', 'CSS', 'TYPESCRIPT', 'ANGULAR', 'ANGULAR MATERIAL', 'PRIMEFLEX'],
+      repositoryUrl: 'https://github.com/sebastian-VB/heores-app'
+    },
+  ];
 }
